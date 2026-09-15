87a99a942cd11572e9a68bb8e2bb0ce3c578d23a
Saharat wichian <chokilover3@gmail.com>
2026-09-15 10:15:56 +0700

    Revert "fix flake8 lint failure"
    This reverts commit c206b7e8d437d592524e64a661e63e5c866f2c43.

--- worker/router_client.py
+++ worker/router_client.py
@@ -1,8 +1,5 @@
 from netmiko import ConnectHandler
-import ntc_templates
-import os
-import json
-
+import ntc_templates, os, json
 
 def get_interfaces(ip, username, password):
 
@@ -26,5 +23,5 @@ def get_interfaces(ip, username, password):
     return result
 
 
-if __name__ == '__main__':
+if __name__=='__main__':
     get_interfaces()
